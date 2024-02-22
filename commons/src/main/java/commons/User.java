6c7cf4bd9aef3a4bcd7bf4eb45b5329b97ec9661
Daria Condratov <d.condratov@student.tudelft.nl>
2024-02-22 15:06:19 +0100

    User

--- commons/src/main/java/commons/User.java
+++ commons/src/main/java/commons/User.java
@@ -1,6 +1,7 @@
 package commons;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Optional;
 
 public class User {
@@ -27,8 +28,43 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void editUsername(String username) {
         this.username = username;
     }
+    public List<Event> getEvents() {
+        return events;
+    }
 
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+
+    public Optional<String> getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = Optional.of(bankAccount);
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void switchLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getUsername(), user.getUsername()) && Objects.equals(getEvents(), user.getEvents()) && Objects.equals(getBankAccount(), user.getBankAccount()) && Objects.equals(getLanguage(), user.getLanguage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getEvents(), getBankAccount(), getLanguage());
+    }
 }
