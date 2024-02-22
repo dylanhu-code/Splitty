e16da7f404d45b952f44623665be22676ed835f7
Daria Condratov <d.condratov@student.tudelft.nl>
2024-02-22 16:52:44 +0100

    UserTest

--- commons/src/main/java/commons/User.java
+++ commons/src/main/java/commons/User.java
@@ -1,6 +1,7 @@
 package commons;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -38,6 +39,9 @@ public class User {
     public void addEvent(Event event) {
         events.add(event);
     }
+    public void removeEvent(Event event){
+        events.remove(event);
+    }
 
     public Optional<String> getBankAccount() {
         return bankAccount;
@@ -67,4 +71,31 @@ public class User {
     public int hashCode() {
         return Objects.hash(getUsername(), getEvents(), getBankAccount(), getLanguage());
     }
+
+    public void editEventTitle(Event event, String name){
+        event.setName(name);
+    }
+    public void createExpense(Event event, int amount, String title, List<User> people){
+        Expense expense = new Expense(amount, title, this, people);
+        event.addExpense(expense);
+    }
+    public void removeExpense(Event event, Expense expense){
+        event.removeExpense(expense);
+    }
+    public void editExpenseAmount(Event event, Expense expense, int amount){
+        event.removeExpense(expense);
+        expense.setAmount(amount);
+        event.addExpense(expense);
+    }
+    public void editExpenseTitle(Event event, Expense expense, String title){
+        event.removeExpense(expense);
+        expense.setName(title);
+        event.addExpense(expense);
+    }
+    public void editExpenseParticipants(Event event, Expense expense, List<User> people){
+        event.removeExpense(expense);
+        expense.setParticipants(people);
+        event.addExpense(expense);
+    }
+
 }
