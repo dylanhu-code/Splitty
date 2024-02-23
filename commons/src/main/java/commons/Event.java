0dc46b9e5e31133c1ae2f45fe37e178537770840
Tigo Schutgens <t.r.schutgens@student.tudelft.nl>
2024-02-23 12:21:04 +0100

    changed some stuff regarding the id like adding the static id, also added javadoc

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -2,10 +2,13 @@ package commons;
 
 import java.util.*;
 
-
+/**
+ * Event class, used to manage expenses done within a group of people
+ */
 public class Event {
     private String title;
-    private String inviteCode;
+    private static int staticId = 0;
+    private final int id;
     private ArrayList<User> participantList;
     private ArrayList<Debt> debtList;
     private ArrayList<Expense> expenseList;
@@ -15,65 +18,101 @@ public class Event {
         participantList = new ArrayList<User>();
         debtList = new ArrayList<Debt>();
         expenseList = new ArrayList<Expense>();
+        this.id = staticId++;
     }
 
+    /**
+     * add user as participant to the event
+     * @param user to add
+     */
     public void addParticipant(User user){
         participantList.add(user);
     }
-
+    /**
+     * remove user from the event
+     * @param user to remove
+     */
     public void removeParticipant(User user){
         participantList.remove(user);
     }
-
+    /**
+     * add debt to the event
+     * @param debt to add
+     */
     public void addDebt(Debt debt){
         debtList.add(debt);
     }
 
+    /**
+     * remove a debt from the event
+     * @param debt to remove
+     */
     public void removeDebt(Debt debt){
         debtList.remove(debt);
     }
+    /**
+     * add expense to the list of expenses of the event
+     * @param expense to add
+     */
+    public void addExpense(Expense expense){
+        expenseList.add(expense);
+    }
+    /**
+     * remove expense from the list of expenses of the event
+     * @param expense to remove
+     */
+    public void removeExpense(Expense expense){
+        expenseList.remove(expense);
+    }
+
+    /**
+     * get the title of an event
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
-
-    public String getInviteCode() {
-        return inviteCode;
+    /**
+     * get the id of an event
+     * @return id
+     */
+    public int getId() {
+        return id;
     }
 
+    /**
+     * get the static id
+     * @return static id
+     */
+    public int getStaticId() {
+        return staticId;
+    }
+    /**
+     * get the list of participants of an event
+     * @return participant list
+     */
     public ArrayList<User> getParticipants() {
         return participantList;
     }
-
+    /**
+     * get the list of debts of an event
+     * @return debt list
+     */
     public ArrayList<Debt> getDebts() {
         return debtList;
     }
-
+    /**
+     * get the list of expenses of an event
+     * @return expense list
+     */
     public ArrayList<Expense> getExpenses() {
         return expenseList;
     }
-
+    /**
+     * set the title of an event
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setInviteCode(String inviteCode) {
-        this.inviteCode = inviteCode;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return Objects.equals(title, event.title) &&
-            Objects.equals(inviteCode, event.inviteCode) &&
-            Objects.equals(participantList, event.participantList) &&
-            Objects.equals(debtList, event.debtList) &&
-            Objects.equals(expenseList, event.expenseList);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, inviteCode, participantList, debtList, expenseList);
-    }
 }
