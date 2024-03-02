9bfe30e627ca263ffb9002292c5fe4d237aaeb31
Dylan Hu <D.Hu-4@student.tudelft.nl>
2024-03-02 20:31:47 +0100

    Initialization of the ctrl class and the scene

--- client/src/main/java/client/scenes/AddExpenseCtrl.java
+++ client/src/main/java/client/scenes/AddExpenseCtrl.java
@@ -1,4 +1,34 @@
 package client.scenes;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+/**
+ * Controller class for the Add Expense view.
+ */
 public class AddExpenseCtrl {
+
+    @FXML
+    private ChoiceBox<String> whoPaidChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> expenseTypeChoiceBox;
+
+
+    /**
+     * Initializes the controller.
+     */
+    @FXML
+    public void initialize() {
+        initChoiceBoxes();
+
+    }
+
+    /**
+     * Initializes the choice boxes with available options.
+     */
+    private void initChoiceBoxes() {
+        whoPaidChoiceBox.getItems().addAll("Person A", "Person B", "Person C");
+        expenseTypeChoiceBox.getItems().addAll("Type 1", "Type 2", "Type 3");
+    }
 }
