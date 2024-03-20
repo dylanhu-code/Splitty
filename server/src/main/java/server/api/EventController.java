561ddb86baa30eb19f49c9de291597f495af9929
Dylan Hu <D.Hu-4@student.tudelft.nl>
2024-03-20 16:18:29 +0100

    Web socket config and methods in eventcontroller

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -5,6 +5,8 @@ import commons.Event;
 import commons.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 import server.services.EventService;
 
@@ -95,10 +97,22 @@ public class EventController {
     }
 
     /**
-     * Add a participant to an Event
-     * @param id
-     * @param participant
-     * @return Ok message with updated event or errorcode
+     * Add a participant to an Event using Websockets
+     * @param id The ID of the event
+     * @param participant The participant to add
+     * @return The updated event
+     */
+    @MessageMapping("/event/join")
+    @SendTo("/topic/event/update")
+    public Event joinEvent(Long id, User participant) {
+       return addParticipant(id, participant).getBody();
+    }
+
+    /**
+     * Add a participant to an Event via REST API
+     * @param id The ID of the event
+     * @param participant The participant to add
+     * @return Ok message containing the updated event or an error code
      */
     @PutMapping(path = {"/addParticipant/{id}"})
     public ResponseEntity<Event> addParticipant(@PathVariable long id,
@@ -119,10 +133,22 @@ public class EventController {
     }
 
     /**
-     * removes one single participant
-     * @param id
-     * @param participant
-     * @return Ok message with updated event or errorcode
+     * Remove a participant from an Event using Websockets
+     * @param id The ID of the event
+     * @param participant The participant to remove
+     * @return The updated event
+     */
+    @MessageMapping("/event/leave")
+    @SendTo("/topic/event/update")
+    public Event leaveEvent(Long id, User participant) {
+        return removeParticipant(id, participant).getBody();
+    }
+
+    /**
+     * Remove a participant from an Event via REST API
+     * @param id The ID of the event
+     * @param participant The participant to remove
+     * @return Ok message containing the updated event or an error code
      */
     @DeleteMapping(path = {"/removeParticipant/{id}"})
     public ResponseEntity<Event> removeParticipant(@PathVariable long id,
@@ -141,7 +167,8 @@ public class EventController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-                                                /**
+
+    /**
      * Orders events by creation date
      * @return - ok message or error message
      */
