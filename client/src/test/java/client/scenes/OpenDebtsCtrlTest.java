d2a98d221c09f9621b45cec8a7db912c043bad3a
Dylan Hu <D.Hu-4@student.tudelft.nl>
2024-04-01 22:06:17 +0200

    Fixed failing test and checkstyle

--- client/src/test/java/client/scenes/OpenDebtsCtrlTest.java
+++ client/src/test/java/client/scenes/OpenDebtsCtrlTest.java
@@ -2,13 +2,8 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 class OpenDebtsCtrlTest {
     private OpenDebtsCtrl ctrl;
