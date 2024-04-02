6b6a66888821345cbc3046410958893d73cb2c3a
Mireia Carrio Cortada <M.Carrio@student.tudelft.nl>
2024-04-02 16:48:04 +0200

    Statistics fxml updated

--- client/src/main/java/client/scenes/StatisticsCtrl.java
+++ client/src/main/java/client/scenes/StatisticsCtrl.java
@@ -1,14 +1,20 @@
 package client.scenes;
 
 import client.utils.StatisticsUtils;
+import com.google.inject.Inject;
 import commons.Event;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -20,10 +26,23 @@ public class StatisticsCtrl {
 
     @FXML
     private Label totalExpenseLabel;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Label eventTitle;
+    @FXML
+    private VBox legend;
     private Scene statistics;
     private Stage primaryStage;
     private Event event;
-    private StatisticsUtils utils = new StatisticsUtils();
+    private final SplittyMainCtrl mainCtrl;
+    private final StatisticsUtils utils;
+
+    @Inject
+    public StatisticsCtrl(SplittyMainCtrl mainCtrl, StatisticsUtils utils) {
+        this.mainCtrl = mainCtrl;
+        this.utils = utils;
+    }
 
     /**
      * Initializes the statistics page
@@ -36,9 +55,12 @@ public class StatisticsCtrl {
         this.event = event;
         this.primaryStage = primaryStage;
         this.statistics = statistics;
+        eventTitle.setText(event.getTitle());
+        var pieData = utils.generatePieChartData(event.getExpenses());
         updateTotalExpense(utils.calculateTotalExpense(event.getExpenses()));
-        updatePieChartData(utils.generatePieChartData(event.getExpenses()));
-
+        updatePieChartData(pieData);
+        utils.setSliceColors(pieChart, event.getExpenses());
+        utils.createLegend(legend, event.getExpenses());
         primaryStage.setScene(statistics);
         primaryStage.show();
     }
@@ -56,6 +78,10 @@ public class StatisticsCtrl {
      * @param totalExpense - new expense amount
      */
     public void updateTotalExpense(double totalExpense) {
-        totalExpenseLabel.setText("Total Expense Amount: " + totalExpense);
+        totalExpenseLabel.setText(String.valueOf(totalExpense));
+    }
+    public void goBack() {
+        mainCtrl.showOverview(event);
+
     }
 }
